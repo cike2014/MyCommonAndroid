5755a7881a9bee051c1788ec499bcbfab0f3f610
debert <1193452522@qq.com>
2016-08-25 12:19:02 +0800

    MainActivity

--- app/src/main/java/com/mmednet/common/MainActivity.java
+++ app/src/main/java/com/mmednet/common/MainActivity.java
@@ -1,9 +1,13 @@
 package com.mmednet.common;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.mmednet.common.multithread.MultiThreadActivity;
+import com.mmednet.common.process.ProcessActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,8 +29,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(v.getId()){
             case R.id.bt_multithread:
 
+                startActivity(new Intent(this, MultiThreadActivity.class));
+
                 break;
             case R.id.bt_process:
+
+                startActivity(new Intent(this, ProcessActivity.class));
 
                 break;
         }
